015799ae362a3b1fea8667d80f4da724328679fd
Aswathi Suseelan <aswathi.suseelan@AD.MSYSTECHNOLOGIES.COM>
2022-12-07 16:54:47 +0530

    added error boundary & readme

--- src/components/ContainerPage/index.js
+++ src/components/ContainerPage/index.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import ErrorContainer from "../../containers/ErrorContainer";
+import "./index.css";
 
-const Header = React.lazy(() => import('HeaderAndFooter/Header'))
-const TopRepos = React.lazy(() => import('TopRepos/TopRepos'))
-const Activities = React.lazy(() => import('Activities/CommitsCard'))
+const Header = React.lazy(() => import("HeaderAndFooter/Header"));
+const TopRepos = React.lazy(() => import("TopRepos/TopRepos"));
+const Activities = React.lazy(() => import("Activities/StarredRepos"));
 
+/**
+ * Container Page
+ *
+ * @description: Container Page containing all the MFEs
+ * @returns Combined MFEs
+ */
 function ContainerPage() {
-  return (
-    <>
-      <Header />
-      <div className="d-flex justify-content-evenly m-4 my-container">
-        <TopRepos />
-        <Activities />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <ErrorContainer>
+                <Header />
+            </ErrorContainer>
+            <div className="d-flex justify-content-evenly m-4 my-container">
+                <ErrorContainer>
+                    <TopRepos />
+                </ErrorContainer>
+                <ErrorContainer>
+                    <Activities />
+                </ErrorContainer>
+            </div>
+        </>
+    );
 }
 
 export default ContainerPage;
