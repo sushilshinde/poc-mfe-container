4d99ade044a6b93cb9523d886006d663f1416c0e
aswathi-suseelan <asuseelan@msystechnologies.com>
2023-01-25 18:07:26 +0530

    added logout url for env

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const dotenv = require('dotenv');
 const { dependencies } = require('./package.json')
 
-const env = dotenv.config().parsed || { API_URL: 'https://poc-mfe-github-api.cyclic.app' }// dotenv.config().parsed
+const env = dotenv.config().parsed || { API_URL: 'https://poc-mfe-github-api.cyclic.app', LOGOUT_URL : 'http://localhost:3000/logout'}// dotenv.config().parsed
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
