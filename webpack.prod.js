a2a1cd11fe9d92dba12ae7dbaac80ac3152dad2d
aswathi-suseelan <asuseelan@msystechnologies.com>
2023-01-27 13:28:41 +0530

    env prod url for mfes added

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const dotenv = require('dotenv');
 const { dependencies } = require('./package.json')
 
-const env = dotenv.config().parsed || { API_URL: 'https://poc-mfe-github-api.cyclic.app', LOGOUT_URL : 'http://localhost:3000/logout'}// dotenv.config().parsed
+const env = dotenv.config().parsed;// dotenv.config().parsed
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
@@ -47,11 +47,11 @@ module.exports = {
         name: "Host",
         filename: "moduleEntry.js",
         remotes: {
-          Login: `Login@http://localhost:3005/remoteEntry.js`,
-          HeaderAndFooter: `HeaderAndFooter@https://poc-mfe-header-footer.onrender.com/moduleEntry.js`,
-          TopRepos: `TopRepos@https://poc-mfe-top-repos.onrender.com/moduleEntry.js`,
-          Activities: `Activities@https://poc-mfe-activities.onrender.com/moduleEntry.js`,
-          PublicEvents: `PublicEvents@https://poc-mfe-events.onrender.com/remoteEntry.js`
+          Login: env.LOGIN_PAGE,
+          HeaderAndFooter: env.HEADER_FOOTER_PAGE,
+          TopRepos: env.TOP_REPOS_PAGE,
+          Activities: env.ACTIVITIES_PAGE,
+          PublicEvents: env.PUBLIC_EVENTS_PAGE
         },
         shared: {
             ...dependencies,
